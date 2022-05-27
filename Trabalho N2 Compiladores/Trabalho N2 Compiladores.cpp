e0987391093e348c21e5cc13c6df48122266fb1b
Herik Sarmento <herik@ucl.br>
2022-05-27 13:51:14 -0300

    string,char e finalizações de identificadores e numeros

--- Trabalho N2 Compiladores/Trabalho N2 Compiladores.cpp
+++ Trabalho N2 Compiladores/Trabalho N2 Compiladores.cpp
@@ -24,7 +24,6 @@ void ImprimirToken(string tipo,string token) {
 }
 
 bool VerificarPalavraReservada(string t) {
-    bool verificador;
     for (int i = 0; i < 16; i++)
     {
         if (t==palavrasReservadas[i])
@@ -42,9 +41,11 @@ int main() {
     ifstream leitura;
     ofstream escrita;
     char aux;
-    int estado=1;
+    int estado = 1;
     string token;
-    
+    int c = 0;
+    int max = 1;
+
     token = "";
     //const string caminho = "C:\\Users\\HerikSarmento\\Desktop\\fonte.txt";
     leitura.open("fonte.txt", ios::in);
@@ -60,58 +61,106 @@ int main() {
             if (aux == ' ') {
                 estado = 1;
             }
-            else if (VerificarLetra(aux)){
+            else if (VerificarLetra(aux)) {
                 estado = 2;
                 token = token + aux;
             }
-            else if (VerificarNumero(aux))
-            {
+            else if (VerificarNumero(aux)){
                 estado = 3;
                 token = token + aux;
             }
+            else if (aux == '"') {
+                estado = 4;
+            }
+            else if (aux == 39) { // Aspas simples
+                estado = 5;
+            }
+
             break;
+
         case 2:
             if (VerificarLetra(aux) || VerificarNumero(aux)) {
                 estado = 2;
                 token = token + aux;
             }
-            else if (aux == ' ') { //Finalização da cadeia
-                if (VerificarPalavraReservada(token)) // Verificação se é uma palavra reservada
-                {
-                    ImprimirToken(token,"");
+            else if (((aux >= 32) && (aux <= 33)) || ((aux >= 40) && (aux <= 47)) || ((aux >= 59) && (aux <= 62))) { // Após um identificador pode ter os seguintes simbolos ' ' '!' '(' ')' '*' '+' ',' '-' '.' '/' ';' '<' '=' '>' 
+                if (VerificarPalavraReservada(token)){ // Verificação se é uma palavra reservada
+                    ImprimirToken(token, "");
                 }
                 else {                                //Não é uma palavra reservada então é um identificador  
                     ImprimirToken("id", token);
+                    if (aux != ' '){
+                        token = aux;
+                        ImprimirToken(token, "");
+                        token = "";
+                    }
                 }
                 token = "";
                 estado = 1;
             }
             break;
+
         case 3:
             if (VerificarNumero(aux)) {
                 estado = 3;
                 token = token + aux;
             }
-            else if (aux== ' ') {
+            else if ((aux == ' ') || ((aux >= 41) && (aux <= 47)) || ((aux >= 58) && (aux <= 62))) {  // Após um numero pode ter os seguintes simbolos: ' ' ')' '*' '+' ',' '-' '.' '/' ':' ';' '<' '=' '>' 
                 ImprimirToken("numero", token);
                 estado = 1;
                 token = "";
+                if (aux != ' ')
+                {
+                    token = aux;
+                    ImprimirToken(token, "");
+                    token = "";
+                }
             }
-            else {
+            else {                                  //Letra após o numero é um erro
                 cout << "(erro,);";
                 estado = 1;
                 token = "";
             }
-            
+
             break;
 
-
-        default:
-            
+        case 4:
+            if (aux != '"') {
+                token = token + aux;
+            }
+            else {
+                ImprimirToken("literal", token);
+                estado = 1;
+                token = "";
+            }
             break;
+
+        case 5:
+            if (aux != 39) {
+                token = token + aux;
+                c++;
+                if (aux == 92) {  // Caso seja contra barra '\' pode ter dois caracteres, caso não apenas um 
+                    max = 2;
+                }
+            }
+            else {
+                if (c == max) {
+                    ImprimirToken("char", token);
+                    max = 1;
+                }
+                else {
+                    ImprimirToken("erro", "");
+                    
+                }
+                token = "";
+                estado = 1;
+                c = 0;
+            }
+                break;
+
+            }
+            //leitura.get(aux);
         }
-        //leitura.get(aux);
-    }
-    leitura.close();
+        leitura.close();
 
-}
+    }
