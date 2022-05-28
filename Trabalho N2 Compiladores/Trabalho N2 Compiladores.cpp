10fc3c197774c257a182c8b6bb72497dc0a2f1e0
Herik Sarmento <herik@ucl.br>
2022-05-28 15:42:33 -0300

    comentários,operadores,outros simbolos e mais maneiras de finalizar um identificador/numero

--- Trabalho N2 Compiladores/Trabalho N2 Compiladores.cpp
+++ Trabalho N2 Compiladores/Trabalho N2 Compiladores.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 const string palavrasReservadas[] = { "break","char","const","continue","default","do","double","else","float","for","if","int","long","return","void","while" };
 
@@ -20,9 +21,8 @@ bool VerificarNumero(char c) {
     }
 }
 void ImprimirToken(string tipo,string token) {
-    cout << "(" << tipo << "," << token << ");";
+    cout << "[" << tipo << "," << token << "];";
 }
-
 bool VerificarPalavraReservada(string t) {
     for (int i = 0; i < 16; i++)
     {
@@ -34,69 +34,122 @@ bool VerificarPalavraReservada(string t) {
     }
     return false;
 }
+bool VerificarOperador(char c) {
+    char op[] = { '+','-','*', '/','=','&','!','|','>','<'};
+    for (int i = 0; i < 10; i++)
+    {
+        if (c == op[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+bool VerificarOperadorRelacional(string r) {
+    string rel[] = { "==","+=","-=","!=",">=","<=","++","--","||","&&"};
+    for (int i = 0; i < 10; i++)
+    {
+        if (r == rel[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+bool VerificarOutrosCaracteres(char c) {
+    char id[] = {'(',')','{','}',';',','};
+    for (int i = 0; i < 6; i++)
+    {
+        if (c == id[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+
 
 
 
 int main() {
     ifstream leitura;
     ofstream escrita;
-    char aux;
+    char aux=' ';
     int estado = 1;
     string token;
     int c = 0;
     int max = 1;
-
     token = "";
-    //const string caminho = "C:\\Users\\HerikSarmento\\Desktop\\fonte.txt";
+    bool auth=false;
     leitura.open("fonte.txt", ios::in);
     while (!leitura.eof()) {
-        leitura.get(aux);
-        if (leitura.eof())
+        
+        if ((aux >= 0) && (aux <= 31)) { // Caso o caractere lido seja algum 
+            estado = 1;
+            token = "";
+            auth = true;
+        }
+        if (auth) {         // Ao finalizar um token um caractere pode ficar sem ser analisado para que isso não ocorra só lerá o proximo caractere no proximo loop
+            leitura.get(aux);
+        }
+        auth = true;
+        if (leitura.eof())  //  É necessário pois quando encontrar o final do arquivo a variavel aux que está recebendo não atualizará
         {
             aux = ' ';
-        }
+        }   
         switch (estado)
         {
         case 1:
             if (aux == ' ') {
                 estado = 1;
             }
-            else if (VerificarLetra(aux)) {
+            else if (VerificarLetra(aux)) { // Estado que fará a verificação se é um identificador ou palavra reservada
                 estado = 2;
                 token = token + aux;
             }
-            else if (VerificarNumero(aux)){
+            else if (VerificarNumero(aux)){ // Estado que fará a verificação se é um numero
                 estado = 3;
                 token = token + aux;
             }
-            else if (aux == '"') {
+            else if (aux == '"') {          // string " " 
                 estado = 4;
-            }
-            else if (aux == 39) { // Aspas simples
+            }   
+            else if (aux == 39) {           // char ' '
                 estado = 5;
+            }
+            else if (VerificarOperador(aux)) {      // Operadores 
+                estado = 6;
+                token = token + aux;
+            }
+            else if (VerificarOutrosCaracteres(aux))     // Resto dos caracteres ( ) , ; {  }
+            {
+                token = token + aux;
+                ImprimirToken(token, "");
+                token = "";
             }
 
             break;
 
         case 2:
-            if (VerificarLetra(aux) || VerificarNumero(aux)) {
+            if (VerificarLetra(aux) || VerificarNumero(aux)){
                 estado = 2;
                 token = token + aux;
             }
-            else if (((aux >= 32) && (aux <= 33)) || ((aux >= 40) && (aux <= 47)) || ((aux >= 59) && (aux <= 62))) { // Após um identificador pode ter os seguintes simbolos ' ' '!' '(' ')' '*' '+' ',' '-' '.' '/' ';' '<' '=' '>' 
+            else{ 
                 if (VerificarPalavraReservada(token)){ // Verificação se é uma palavra reservada
-                    ImprimirToken(token, "");
+                    ImprimirToken(token, "");          // Imprime a palavra reservada
                 }
-                else {                                //Não é uma palavra reservada então é um identificador  
+                else {                                 // Não é uma palavra reservada então é um identificador  
                     ImprimirToken("id", token);
-                    if (aux != ' '){
-                        token = aux;
-                        ImprimirToken(token, "");
-                        token = "";
-                    }
                 }
                 token = "";
                 estado = 1;
+                if (aux != ' ') {
+                    auth = false;
+                }
             }
             break;
 
@@ -105,23 +158,14 @@ int main() {
                 estado = 3;
                 token = token + aux;
             }
-            else if ((aux == ' ') || ((aux >= 41) && (aux <= 47)) || ((aux >= 58) && (aux <= 62))) {  // Após um numero pode ter os seguintes simbolos: ' ' ')' '*' '+' ',' '-' '.' '/' ':' ';' '<' '=' '>' 
+            else {  
                 ImprimirToken("numero", token);
                 estado = 1;
                 token = "";
-                if (aux != ' ')
-                {
-                    token = aux;
-                    ImprimirToken(token, "");
-                    token = "";
+                if (aux != ' ') {
+                    auth = false;
                 }
             }
-            else {                                  //Letra após o numero é um erro
-                cout << "(erro,);";
-                estado = 1;
-                token = "";
-            }
-
             break;
 
         case 4:
@@ -138,28 +182,67 @@ int main() {
         case 5:
             if (aux != 39) {
                 token = token + aux;
-                c++;
-                if (aux == 92) {  // Caso seja contra barra '\' pode ter dois caracteres, caso não apenas um 
-                    max = 2;
-                }
             }
             else {
-                if (c == max) {
-                    ImprimirToken("char", token);
-                    max = 1;
+                ImprimirToken("char", token);
+                token = "";
+                estado = 1;
+                if (aux != ' ') {
+                    auth = false;
+                }
+            }
+                break;
+        case  6:
+            if ((token == "/")&&(aux=='/')) {
+                estado = 7;
+                token = "";
+            }
+            else if ((token == "/") && (aux == '*')) {
+                estado = 8;
+                token = "";
+            }
+            else {
+                if (VerificarOperador(aux)) {
+                    if (VerificarOperadorRelacional(token + aux)) {
+                        token = token + aux;
+                        ImprimirToken(token, "");
+                    }
+                    else {
+                        ImprimirToken(token, "");
+                        token = "";
+                        token = token + aux;
+                        ImprimirToken(token, "");
+                    }
                 }
                 else {
-                    ImprimirToken("erro", "");
-                    
+                    ImprimirToken(token, "");
+                    if (aux != ' ') {
+                        auth = false;
+                    }
                 }
                 token = "";
                 estado = 1;
-                c = 0;
             }
-                break;
+            break;
+
+        case 7:
+            if (aux == '\n') {
+                estado = 1;
+                }
+            break;
+
+        case  8:
+            if (aux == '*') {
+                token = "";
+                token = token + aux;
+            }
+            if ((token == "*") && (aux == '/')) {
+                estado = 1;
+                token = "";
+            }
+            break;
 
             }
-            //leitura.get(aux);
         }
         leitura.close();
 
